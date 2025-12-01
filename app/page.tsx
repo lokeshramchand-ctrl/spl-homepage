5d21453bb922bf8d89cfcb8c96f9484cfbdf6452
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-12-01 09:00:38 +0530

    refactor: Remove unused imports from Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
-import Hero from '@/components/home/Hero';
-import StatsSection from '@/components/home/StatsSection';
-import Features from '@/components/home/Features';
-import Industries from '@/components/home/Industries';
-import FAQ from '@/components/home/FAQ';
 
 
 export default function Home() {
