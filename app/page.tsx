3e500bfa6f940b10b15e8993fb1bce6483beb01c
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-12-01 09:01:19 +0530

    feat: Add imports for FAQ, Features, Hero, Industries, and StatsSection components in Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
+import FAQ from "./components/home/FAQ";
+import Features from "./components/home/Features";
+import Hero from "./components/home/Hero";
+import Industries from "./components/home/Industries";
+import StatsSection from "./components/home/StatSections";
 
 
 export default function Home() {
