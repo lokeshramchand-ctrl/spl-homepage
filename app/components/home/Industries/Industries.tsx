78521c8dc0c80ae429c5cb98e41f19be5cfd7800
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-12-02 21:56:01 +0530

    refactor: Remove Osmo credits section from Industries component and global styles

--- app/components/home/Industries/Industries.tsx
+++ app/components/home/Industries/Industries.tsx
@@ -129,10 +129,6 @@ export default function Industries() {
     </div>
     </div>
   </section>
-<div className="osmo-credits">
-  <p className="osmo-credits__p">Resource by <a target="_blank" href="https://www.osmo.supply/" className="osmo-credits__p-a">Osmo</a>
-  </p>
-</div>
     </>
   );
 }
