6b98284317a5c1e2f314d14b66356ec43f139259
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2026-01-20 17:34:33 +0000

    Refactor SVG imports in CarouselVertical and add new spl-inkscape-side.svg

--- app/components/home/CarouselVertical.tsx
+++ app/components/home/CarouselVertical.tsx
@@ -2,8 +2,8 @@
 import type React from "react";
 import { useEffect, useState } from "react";
 import Image, { StaticImageData} from "next/image"
-import InvoiceSvg from "../svg/Finance";
-import BadgeSvg from "../svg/Education";
+import InvoiceSvg from "../svg/Invoice";
+import BadgeSvg from "../svg/Badge";
 
 export type SvgComponent = (
   props: React.SVGProps<SVGSVGElement>
