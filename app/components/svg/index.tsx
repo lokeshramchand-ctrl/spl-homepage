c2cac0cb868e13cd167c3b6818d86922e40924e4
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2026-01-20 17:31:42 +0000

    Refactor SVG imports and declarations for improved organization and clarity

---
+++ app/components/svg/index.tsx
@@ -0,0 +1,15 @@
+import type { SVGProps } from 'react';
+import FinanceIcon from "../../assets/finance.svg";
+import FinanceStrategyIcon from "../../assets/finance--strategy.svg";
+import EducationIcon from "../../assets/education.svg";
+import BadgeIcon from "../../assets/badge.svg";
+import InvoiceIcon from "../../assets/invoice.svg";
+import RealEstateIcon from "../../assets/real-estate.svg";
+import GovernmentIcon from "../../assets/govern--users.svg";
+import HomeFrontIcon from "../../assets/home--front.svg";
+
+export type IconProps = SVGProps<SVGSVGElement> & {
+  size?: number;
+};
+
+export { FinanceIcon, FinanceStrategyIcon, EducationIcon, GovernmentIcon, HomeFrontIcon, BadgeIcon, InvoiceIcon, RealEstateIcon };
--- app/components/svg/main.ts
+++
@@ -1,8 +0,0 @@
-import FinanceIcon from "../../assets/finance.svg";
-import FinanceStrategyIcon from "../../assets/finance--strategy.svg";
-import EducationIcon from "../../assets/education.svg";
-
-import GovernmentIcon from "../../assets/govern--users.svg";
-import HomeFrontIcon from "../../assets/home--front.svg";
-
-export { FinanceIcon, FinanceStrategyIcon, EducationIcon, GovernmentIcon, HomeFrontIcon };
